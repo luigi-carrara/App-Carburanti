6f73c745cd06530e57ce7642f406d5826d064a8a
Luigi Carrara <34343610+luigi-carrara@users.noreply.github.com>
2026-03-31 23:24:04 +0200

    Aggiunto token chiamate API

--- api.py
+++ api.py
@@ -11,6 +11,7 @@ config_path = os.path.join(base_path, "config.env")
 load_dotenv(config_path)
 
 SERVER_IP = os.getenv("SERVER_IP", "localhost")
+API_KEY_EXPECTED = os.getenv("API_TOKEN", 'NULL')
 
 app = FastAPI(title="API Prezzi Carburanti Mugnano")
 
